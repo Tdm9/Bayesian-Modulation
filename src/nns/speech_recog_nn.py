279db508221b99ed8dfbb59a1e3cc1452298718c
dleandro <diogo_leandrao@hotmail.com>
2019-12-23 16:33:03 +0000

    neural_nets_and_speech_recog word modified now has index and front page, introduction added and a results photo added as well, code is functional

---
+++ src/nns/speech_recog_nn.py
@@ -0,0 +1,131 @@
+from preprocess import *
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, LSTM
+from keras.utils import to_categorical
+import wandb
+from wandb.keras import WandbCallback
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow.keras.layers as KL
+
+wandb.init()
+config = wandb.config
+
+config.max_len = 11
+config.buckets = 20
+
+# Save data to array file first
+save_data_to_array(max_len=config.max_len, n_mfcc=config.buckets)
+
+labels=["bed", "happy", "cat"]
+
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+channels = 1
+config.epochs = 50
+config.batch_size = 100
+
+num_classes = 3
+
+X_train = X_train.reshape(X_train.shape[0], config.buckets, config.max_len, channels)
+X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len, channels)
+
+plt.imshow(X_train[100, :, :, 0])
+print(y_train[100])
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+'''
+# Simple Perceptron
+print("Perceptron")
+X_train = X_train.reshape(X_train.shape[0], config.buckets, config.max_len)
+X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len)
+model = Sequential()
+model.add(Flatten(input_shape=(config.buckets, config.max_len)))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])
+wandb.init()
+model.fit(X_train, y_train_hot, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
+'''
+
+'''
+# 2D Convolution
+print("2D Convulutions")
+model = Sequential()
+model.add(Conv2D(32,
+        (3, 3),
+        input_shape=(config.buckets, config.max_len, channels),
+        activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])
+wandb.init()
+model.fit(X_train, y_train_hot, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
+
+'''
+
+'''
+print("Now with 2 Convolutions and dropout because of overfitting")
+model = Sequential()
+model.add(Conv2D(32,
+        (3, 3),
+        input_shape=(config.buckets, config.max_len, channels),
+        activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32,
+        (3, 3),
+        activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])
+wandb.init()
+model.fit(X_train, y_train_hot, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
+
+'''
+# LSTM RNN
+print("LSTM")
+model = Sequential()
+model.add(LSTM(16, input_shape=(config.buckets, config.max_len, channels), activation="sigmoid"))
+model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])
+
+wandb.init()
+model.fit(X_train, y_train_hot, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
+
+# feed forward 
+'''
+print("Feed Forward")
+inputs = KL.Input(shape=(config.buckets, config.max_len))
+l = KL.Flatten()(inputs)
+l = KL.Dense(512, activation=tf.nn.relu)(l)
+outputs = KL.Dense(10, activation=tf.nn.softmax)(l)
+
+model = tf.keras.models.Model(inputs, outputs)
+model.summary()
+model.compile(optimizer="adam",
+                loss="sparse_categorical_crossentropy",
+                metrics=["accuracy"])
+wandb.init()
+model.fit(X_train, y_train, epochs=5)
+'''
